936b63ce39b861c5d9e976d9a628cd67b7e0eb16
Emma-MacK <Emma.MacKenzie2@addenbrookes.nhs.net>
2023-01-12 10:07:14 +0000

    reverted vcf changes in code.sh, so running does not try to download new vcf

--- src/code.sh
+++ src/code.sh
@@ -12,12 +12,17 @@ if [ $skip == false ];
      # Remove .bam extension from bam file name
      bam_prefix="${bam_file%.bam}"
 
-     # Download bam and index files
+     # store vcf file name as a string
+     vcf_input=`dx describe "$[vcf_file]"   name`
+
+     # Download bam, index and vcf files
      dx download "$input_bam" -o "$bam_file"
      dx download "$input_bam_index" -o "${bam_prefix}.bai" 
+     dx download "$vcf_file" -o "$vcf_input"
      
      # download the VCF used by VerifyBAMID from 001
-     dx download project-ByfFPz00jy1fk6PjpZ95F27J:file-G77XQY00jy1ZXgBy1fZF7zBQ
+     # commenting out as vcf file is given as an input
+     # dx download project-ByfFPz00jy1fk6PjpZ95F27J:file-G77XQY00jy1ZXgBy1fZF7zBQ
      # Create output directory
      mkdir -p out/verifybamid_out/QC/
 
@@ -25,7 +30,7 @@ if [ $skip == false ];
      # --ignoreRG; to check the contamination for the entire BAM rather than examining individual read groups
      # --precise; calculate the likelihood in log-scale for high-depth data (recommended when --maxDepth is greater than 20)
      # --maxDepth 1000; For the targeted exome sequencing, --maxDepth 1000 and --precise is recommended.
-     verifyBamID --vcf Omni25_genotypes_1525_samples_v2.b37.PASS.ALL.sites.vcf.gz \
+     verifyBamID --vcf $vcf_input \
           --bam $bam_file \
           --out out/verifybamid_out/QC/$bam_prefix \
           --verbose --ignoreRG --precise --maxDepth 1000
