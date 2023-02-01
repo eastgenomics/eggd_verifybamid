3402f19ad03de1a43489da7bc21d9408ee371917
Emma-MacK <113516771+Emma-MacK@users.noreply.github.com>
2023-02-01 16:06:42 +0000

    Update to bring in line with suggestions from review

--- src/code.sh
+++ src/code.sh
@@ -7,24 +7,21 @@ set -e -x -o pipefail
 if [ $skip == false ]; 
      then
      # Store the bam file name as a string
-     bam_file=`dx describe "${input_bam}" --name`
+     bam_file=`dx describe "$input_bam_name"
 
      # Remove .bam extension from bam file name
      bam_prefix="${bam_file%.bam}"
 
      # store vcf file name as a string
      vcf_input=`dx describe "${vcf_file}" --name`
-
+     
      # Download bam, index and vcf files
      dx download "$input_bam" -o "$bam_file"
      dx download "$input_bam_index" -o "${bam_prefix}.bai" 
      dx download "$vcf_file" -o "$vcf_input"
      
-     # download the VCF used by VerifyBAMID from 001
-     # commenting out as vcf file is given as an input
-     # dx download project-ByfFPz00jy1fk6PjpZ95F27J:file-G77XQY00jy1ZXgBy1fZF7zBQ
      # Create output directory
-     mkdir -p out/verifybamid_out/QC/
+     mkdir -p out/verifybamid_out/
 
      # Call verifyBamID for contamination check. The following notable options are passed:
      # --ignoreRG; to check the contamination for the entire BAM rather than examining individual read groups
@@ -32,10 +29,10 @@ if [ $skip == false ];
      # --maxDepth 1000; For the targeted exome sequencing, --maxDepth 1000 and --precise is recommended.
      verifyBamID --vcf $vcf_input \
           --bam $bam_file \
-          --out out/verifybamid_out/QC/$bam_prefix \
+          --out out/verifybamid_out/$bam_prefix \
           --verbose --ignoreRG --precise --maxDepth 1000
 
-     # Upload results to DNA Nexus
+     # Upload results to DNAnexus
      dx-upload-all-outputs
 fi
 
