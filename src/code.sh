c30837b57da2f126a330c79b12e4cb04a87fdb2f
Emma-MacK <Emma.MacKenzie2@addenbrookes.nhs.net>
2023-02-02 11:28:46 +0000

    add DNAnexus arguments to describe input bams in code.sh

--- src/code.sh
+++ src/code.sh
@@ -8,17 +8,17 @@ if [ $skip == false ];
      then
      
      # Store the bam file name as a string
-     bam_file="$input_bam_name"
+     #bam_file="$input_bam_name"
 
      # Remove .bam extension from bam file name
-     bam_prefix="${bam_file%.bam}"
+     #bam_prefix="${bam_file%.bam}"
 
      # store vcf file name as a string
      vcf_input=`dx describe "${vcf_file}" --name`
      
      # Download bam, index and vcf files
-     dx download "$input_bam" -o "$bam_file"
-     dx download "$input_bam_index" -o "${bam_prefix}.bai"
+     dx download "$input_bam" -o "$input_bam_name"
+     dx download "$input_bam_index" -o "${input_bam_prefix}.bai"
      dx download "$vcf_file" -o "$vcf_input"
      
      # Create output directory
@@ -29,8 +29,8 @@ if [ $skip == false ];
      # --precise; calculate the likelihood in log-scale for high-depth data (recommended when --maxDepth is greater than 20)
      # --maxDepth 1000; For the targeted exome sequencing, --maxDepth 1000 and --precise is recommended.
      verifyBamID --vcf $vcf_input \
-          --bam $bam_file \
-          --out out/verifybamid_out/$bam_prefix \
+          --bam $input_bam_name \
+          --out out/verifybamid_out/$input_bam_prefix \
           --verbose --ignoreRG --precise --maxDepth 1000
 
      # Upload results to DNAnexus
