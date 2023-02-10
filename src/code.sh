5700699ff943b9ba7abe2ba6e34ae7738b2e2579
Emma-MacK <Emma.MacKenzie2@addenbrookes.nhs.net>
2023-02-10 08:53:16 +0000

    fix formatting errors, vcf built in variable now working

--- src/code.sh
+++ src/code.sh
@@ -6,7 +6,7 @@ set -e -x -o pipefail
 
 dx download "$input_bam" -o "$input_bam_name"
 dx download "$input_bam_index" -o "${input_bam_prefix}.bai"
-dx download "$vcf_file" -o "$input_vcf_name"
+dx download "$vcf_file" -o "$vcf_file_name"
 
 # Create output directory
 mkdir -p out/verifybamid_out/
@@ -15,7 +15,7 @@ mkdir -p out/verifybamid_out/
 # --ignoreRG; to check the contamination for the entire BAM rather than examining individual read groups
 # --precise; calculate the likelihood in log-scale for high-depth data (recommended when --maxDepth is greater than 20)
 # --maxDepth 1000; For the targeted exome sequencing, --maxDepth 1000 and --precise is recommended.
-verifyBamID --vcf $vcf_input_name \
+verifyBamID --vcf $vcf_file_name \
      --bam $input_bam_name \
      --out out/verifybamid_out/$input_bam_prefix \
      --verbose --ignoreRG --precise --maxDepth 1000
