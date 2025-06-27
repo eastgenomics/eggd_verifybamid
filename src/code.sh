e652af9752c6bd46609fe48a6ae137d5a85b901f
Emma-MacK <emma.mackenzie10@nhs.net>
2025-06-27 11:50:40 +0100

    added ref fasta input, convert cram to bam if cram file given, runs successfully

--- src/code.sh
+++ src/code.sh
@@ -5,8 +5,24 @@
 set -e -x -o pipefail
 
 dx download "$input_bam" -o "$input_bam_name"
-dx download "$input_bam_index" -o "${input_bam_prefix}.bai"
+dx download "$input_bam_index" -o "$input_bam_index_name"
 dx download "$vcf_file" -o "$vcf_file_name"
+
+
+# if input_bam_name == cram, convert to bam and reindex?, then use input_bam_prefix.bam later
+
+if [[ $input_bam_name == *.cram ]]; then
+     echo "file is CRAM"
+
+     # to add - try, if file does not exist then error with "reference fasta needed for cram"
+     dx download "$reference_fasta" -o "$reference_fasta_name"
+     dx download "$reference_fasta_index" -o "$reference_fasta_index_name"
+     samtools view -b -T "$reference_fasta_name" -o "$input_bam_prefix".bam "$input_bam_name"
+     samtools index "$input_bam_prefix".bam
+
+fi
+
+
 
 # Create output directory
 mkdir -p out/verifybamid_out/
@@ -16,7 +32,7 @@ mkdir -p out/verifybamid_out/
 # --precise; calculate the likelihood in log-scale for high-depth data (recommended when --maxDepth is greater than 20)
 # --maxDepth 1000; For the targeted exome sequencing, --maxDepth 1000 and --precise is recommended.
 verifyBamID --vcf $vcf_file_name \
-     --bam $input_bam_name \
+     --bam $input_bam_prefix.bam \
      --out out/verifybamid_out/$input_bam_prefix \
      --verbose --ignoreRG --precise --maxDepth 1000
 
